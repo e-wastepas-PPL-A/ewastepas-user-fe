6e9801b9c328460b6b44d9d28b332354abb3e156
syifarizki <rizkisyifa379@gmail.com>
2024-10-23 17:31:05 +0700

    RegisterPage

---
+++ src/components/Input/InputEmail.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+const InputEmail = ({ label, value, onChange }) => {
+  return (
+    <div className="mb-2 relative">
+      <label className="block text-gray-700 font-semibold mb-1">{label}</label>
+      <input
+        type="email"
+        value={value}
+        onChange={onChange}
+        className="w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary"
+        required
+      />
+    </div>
+  );
+};
+
+export default InputEmail;
