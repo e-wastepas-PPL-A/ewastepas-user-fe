9c2889e7dcfddd447ae33c52f2b6ad33c3acb2de
aufaahusniati <aufaahsnt21@gmail.com>
2025-01-14 22:39:14 +0700

    update

--- src/components/About/Ewhale.jsx
+++ src/components/About/Ewhale.jsx
@@ -1,36 +1,28 @@
 import React from "react";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Ewhale = () => {
   return (
-    <div className="flex flex-col px-6 py-10 text-center bg-white md:px-16 lg:px-32">
-      <h1 className="text-2xl font-bold text-blue-900 mb-6 sm:mr-28 lg:text-3xl">
-        Tentang E-Whale
-      </h1>
+    <div className="bg-gray-50 py-12 px-6 sm:py-16 sm:px-8 lg:px-16">
+      <div className="max-w-6xl mx-auto text-center mb-10">
+        <h2 className="text-3xl sm:text-4xl font-bold text-primary mb-4">
+          Tentang E-Whale
+        </h2>
+        <p className="text-base sm:text-lg text-gray-600">
+          Solusi inovatif dalam pengelolaan sampah elektronik.
+        </p>
+      </div>
 
-      {/* Content Section */}
-      <div className="flex flex-col lg:flex-row items-center lg:items-start lg:space-x-8">
-        {/* Images */}
-        <div className="relative mb-6 lg:mb-0">
-          <LazyLoadImage
-            src="/images/sampah.png"
-            alt="Sampah Elektronik"
-            className="max-w-72 sm:min-w-64 -translate-y-0 sm:-translate-y-16 rounded-full object-cover shadow-lg"
-            effect="blur" // Efek blur saat gambar dimuat
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center max-w-6xl mx-auto">
+        <div className="flex justify-center">
+          <img
+            src="/images/waste.jpg"
+            alt="Team working"
+            className="w-full max-w-md sm:max-w-lg md:max-w-none rounded-lg shadow-md"
           />
-          {/* Small Circle */}
-          {/* <LazyLoadImage
-            src="/images/pattern.jpg"
-            alt="Medical Research"
-            className="absolute bottom-0 left-0 w-24 h-24 md:w-32 md:h-32 rounded-full border-4 border-white object-cover shadow-md transform -translate-x-4 translate-y-4 md:-translate-x-6 md:translate-y-6"
-            effect="blur"
-          /> */}
         </div>
 
-        {/* Description */}
-        <div className="text-gray-700 text-center lg:text-left">
-          <p className="text-sm md:text-base leading-relaxed text-justify">
+        <div>
+          <p className="text-gray-700 sm:text-base text-sm mb-6 text-justify">
             E-Whale didirikan dengan visi menciptakan masa depan yang lebih
             hijau melalui pengelolaan limbah elektronik. Nama 'E-Whale'
             terinspirasi dari paus, simbol megah ekosistem laut, yang sering
